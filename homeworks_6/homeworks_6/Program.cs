b093f6c51eef80a5719b89a361a1da26a1617b37
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-03-31 23:37:42 +0500

    toqSon_massiv
    massivdagi n ta toq son

--- homeworks_6/homeworks_6/Program.cs
+++ homeworks_6/homeworks_6/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(" enter K : ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[k * 2];
+            Random random = new Random();
+            for (int i = 0; i < k * 2; i++)
+            {
+                arr[i] = random.Next(k, k * 4);
+                Console.Write(arr[i] + " ");
+
+            }
+            Console.WriteLine(" ");
+            for (int i = 0; i < k * 2; i++)
+            {
+                if (arr[i] % k == 0)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
         }
     }
 }
