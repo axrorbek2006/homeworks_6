cd06581ba717a10be5e37ff85e6d22d531bb043b
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-04-01 22:53:33 +0500

    task_5
    massiv elementlarini yigindisi va kopaytmasi

--- homeworks_6/homeworks_6/Program.cs
+++ homeworks_6/homeworks_6/Program.cs
@@ -4,6 +4,20 @@
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int[] arr = new int[6];
+            Console.Write("Array: ");
+            int sum = 0, multiple = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = random.Next(1, 8);
+                Console.Write(arr[i] + " ");
+                sum += arr[i];
+                multiple *= arr[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine($"massivni yigindisi : {sum}");
+            Console.WriteLine("massivni yigindisi : "+multiple);
             
         }
     }
