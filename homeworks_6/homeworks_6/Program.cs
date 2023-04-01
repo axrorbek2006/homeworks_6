c0f8c7965dc56594ba8072eb14f6a1a229cc778d
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-04-01 23:30:01 +0500

    task_6
    massivdagai 1 - toq songacha bolgan elementlar yigindisi

--- homeworks_6/homeworks_6/Program.cs
+++ homeworks_6/homeworks_6/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int[] arr = new int[10];
+            Console.Write("Array: ");
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                arr[i] = random.Next(1, 15);
+               Console.Write(arr[i] + " ");
+                if (arr[i] % 2 != 0)
+                {
+                    sum++;
+                }
+            }
+            Console.WriteLine();
+            int summa = 0 , summa2 = 0;
+            if (sum > 0)
+            {
+                for (int i = 0; i <=9; i++)
+                {
+                    if (arr[i]%2 == 0)
+                    {
+                        summa += arr[i];
+                    }
+                    else
+                    {
+                        Console.WriteLine("yigindi : " + summa);
+                        break;
+                    }
+                }              
+            }
+            else
+            {
+                for (int i = 1; i <=8; i++)
+                {                   
+                        summa2 += arr[i];                   
+                }
+                Console.WriteLine("t yigindi : "+summa2);               
+            }            
         }
     }
 }
