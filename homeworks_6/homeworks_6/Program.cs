85bb6431f8ea7cf309db5ecfd7699545bab7a811
Axrorbek <122671790+axrorbek2006@users.noreply.github.com>
2023-04-01 16:00:54 +0500

    Update Program.cs

--- homeworks_6/homeworks_6/Program.cs
+++ homeworks_6/homeworks_6/Program.cs
@@ -4,26 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.Write(" enter K : ");
-            int k = Convert.ToInt32(Console.ReadLine());
-
-            int[] arr = new int[k * 2];
-            Random random = new Random();
-            Console.Write(" massiv :  ");
-            for (int i = 0; i < k * 2; i++)
+            console.writeline("hello, world!");
+            console.write("enter a number : ");
+            int n = convert.toint32(console.readline());
+            int[] summ = new int[n];
+            int sanoq = 2, sanoq2 = 0, sum = 0;
+            while (true)
             {
-                arr[i] = random.Next(k, k * 4);
-                Console.Write(arr[i] + " ");
-
-            }
-            Console.WriteLine(" ");
-            Console.Write($"{k} ga karrali sonlar : ");
-            for (int i = 0; i < k * 2; i++)
-            {
-                if (arr[i] % k == 0)
+                if (sanoq % 2 != 0)
                 {
-                    Console.Write(arr[i] + ", ");
+                    summ[sanoq2] = sanoq;
+                    sum++;
+                    sanoq2++;
                 }
+                if (sum == n)
+                {
+                    break;
+                }
+                sanoq++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                console.write($" {summ[i]} , ");
             }
         }
     }
