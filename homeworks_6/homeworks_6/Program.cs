254fbe81701eea614d1d5598c47bb910c8d3b960
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-04-01 22:42:30 +0500

    task_4
    massivdan berilgan sonni topish ,
    contains funksiyasidan foydalanilgan

--- homeworks_6/homeworks_6/Program.cs
+++ homeworks_6/homeworks_6/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int[] arr = new int[16];
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Array: ");
+            for (int i = 0; i < 15; i++)
+            {
+                arr[i] = random.Next(1, 15);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            if (arr.Contains(n))
+            {
+                Console.WriteLine($"{n} is in the array.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not in the array.");
+            }
+
         }
     }
 }
